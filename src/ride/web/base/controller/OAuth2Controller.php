4d990e64b170f242f5b99bb21ca2fc776d635914
Kaya <kayalion@kayalion.net>
2014-11-26 14:35:06 +0100

    catch inactive error

--- src/ride/web/base/controller/OAuth2Controller.php
+++ src/ride/web/base/controller/OAuth2Controller.php
@@ -2,6 +2,9 @@
 
 namespace ride\web\base\controller;
 
+use ride\library\http\Response;
+use ride\library\security\exception\AuthenticationException;
+use ride\library\security\exception\InactiveAuthenticationException;
 use ride\library\security\SecurityManager;
 
 /**
@@ -18,7 +21,25 @@ class OAuth2Controller extends AbstractController {
      */
     public function authorizeAction(SecurityManager $securityManager, $authenticator = null) {
         $code = $this->request->getQueryParameter('code');
-        if ($securityManager->getUser() || $code) {
+        $user = null;
+
+        try {
+            $user = $securityManager->getUser();
+        } catch (InactiveAuthenticationException $exception) {
+            if ($code) {
+                $this->response->setStatusCode(Response::STATUS_CODE_UNPROCESSABLE_ENTITY);
+
+                $this->addError('error.authentication.inactive');
+            }
+        } catch (AuthenticationException $exception) {
+            if ($code) {
+                $this->response->setStatusCode(Response::STATUS_CODE_UNPROCESSABLE_ENTITY);
+
+                $this->addError('error.authentication');
+            }
+        }
+
+        if ($user || $code) {
             $url = $this->request->getBaseUrl();
 
             if ($this->request->hasSession()) {
